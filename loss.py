f2ee3abe152761c098d967b7a2494d13682db316
HEMONCedric <65610302+HEMONCedric@users.noreply.github.com>
2022-10-24 18:15:11 +0200

    Update loss.py

--- loss.py
+++ loss.py
@@ -5,38 +5,34 @@ from utils import gram_matrix
 
 
 class VGGPerceptualLoss(torch.nn.Module):
-    def __init__(self, resize=True):
+    def __init__(self, device):
         super(VGGPerceptualLoss, self).__init__()
         blocks = []
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16].eval())
-        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23].eval())
-        for bl in blocks:
-            for p in bl.parameters():
-                p.requires_grad = False
-        self.blocks = torch.nn.ModuleList(blocks)
+        self.device = device
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[:4])
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[4:9])
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[9:16])
+        blocks.append(torchvision.models.vgg16(pretrained=True).features[16:23])
+        self.blocks = torch.nn.ModuleList(blocks).eval().to(device)
+        self.blocks.requires_grad_(False)
         self.transform = torch.nn.functional.interpolate
-        self.resize = resize
         self.style_layers = [0,1,2,3]
         self.style_layers_weight = [1,1,1,1]
         self.feature_layers = [0,1,2]
         self.feature_layers_weight = [0,0,1]
         self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
         self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
-
-    def compute(self, input, target_style, target_content):
+        self.loss_func = torch.nn.MSELoss(reduction='mean')
+        
+    def prepare(self, input: torch.Tensor):
         input = input.repeat(1, 3, 1, 1)
-        target_style = target_style.repeat(1, 3, 1, 1)
-        target_content = target_content.repeat(1, 3, 1, 1)
-
         input = (input-self.mean) / self.std
-        target_style = (target_style-self.mean) / self.std
-        target_content = (target_content-self.mean) / self.std
-
         input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
-        target_style = self.transform(target_style, mode='bilinear', size=(224, 224), align_corners=False)
-        target_content = self.transform(target_content, mode='bilinear', size=(224, 224), align_corners=False)
+ 
+    def loss_eval(self, input, target_style, target_content):
+        input = prepare(input)
+        target_style = prepare(target_style)
+        target_content = prepare(target_content)
         
         loss_content = 0.0
         loss_style = 0.0
@@ -47,29 +43,28 @@ class VGGPerceptualLoss(torch.nn.Module):
             x = block(x)
             if i in self.feature_layers:
                 z = block(z)
-                loss_content += torch.nn.MSELoss(reduction='mean')(x, z)*self.feature_layers_weight[i]
+                loss_content += self.loss_func(x, z)*self.feature_layers_weight[i]
             if i in self.style_layers:
                 y = block(y)
                 gram_x = gram_matrix(x.double())
                 gram_y = gram_matrix(y.double())
-                loss_style += torch.nn.MSELoss(reduction='mean')(gram_x, gram_y)*self.style_layers_weight[i]
+                loss_style += self.loss_func(gram_x, gram_y)*self.style_layers_weight[i]
         return loss_content, loss_style
 
-    def forward(self, input, target_style, target_content=None):
+    def forward(self, input, target_style, target_content):
         loss_content = 0.0
         loss_style = 0.0
         if len(input.shape)==4:
-            loss_content, loss_style = self.compute(input, target_style, target_content)
-            loss_content = loss_content/10
-            loss_style = loss_style*30
+            loss_content, loss_style = self.loss_eval(input, target_style, target_content)
         else:
             for i in range(input.shape[2]):
-                loss_content_tmp, loss_style_tmp = self.compute(input[0,0,i], target_style[0,0,i], target_content[0,0,i])
+                loss_content_tmp, loss_style_tmp = self.loss_eval(input[:,0,i], target_style[:,0,i], target_content[:,0,i])
                 loss_content += loss_content_tmp
                 loss_style += loss_style_tmp
-            loss_content = loss_content/input.shape[2]/10
-            loss_style = loss_style/input.shape[2]*30
-        # print("Loss content:", loss_content.item(), "Loss style:", loss_style.item())
+            loss_content /= input.shape[2]
+            loss_style /= input.shape[2]
+        loss_content *= 0.1
+        loss_style *= 30
         return loss_content+loss_style
 
 
@@ -79,13 +74,11 @@ class ConvNextPerceptualLoss(nn.Module):
         blocks = []
         self.device = device
         weights = torchvision.models.ConvNeXt_Tiny_Weights.IMAGENET1K_V1
-        blocks.append(torchvision.models.convnext.convnext_tiny(weights=weights).features[:1].eval().to(device=self.device))
-        blocks.append(torchvision.models.convnext.convnext_tiny(weights=weights).features[1:3].eval().to(device=self.device))
-        blocks.append(torchvision.models.convnext.convnext_tiny(weights=weights).features[3:5].eval().to(device=self.device))
-        for bl in blocks:
-            for p in bl.parameters():
-                p.requires_grad = False
-        self.blocks = nn.ModuleList(blocks)
+        blocks.append(torchvision.models.convnext.convnext_tiny(weights=weights).features[:1])
+        blocks.append(torchvision.models.convnext.convnext_tiny(weights=weights).features[1:3])
+        blocks.append(torchvision.models.convnext.convnext_tiny(weights=weights).features[3:5])
+        self.blocks = nn.ModuleList(blocks).eval().to(device=self.device)
+        self.blocks.requires_grad_(False)
         self.transform = nn.functional.interpolate
         self.style_layers = [0,1,2]
         self.style_layers_weight = [0,1,1] 
@@ -94,18 +87,15 @@ class ConvNextPerceptualLoss(nn.Module):
         self.register_buffer("mean", torch.tensor([0.485, 0.456, 0.406]).view(1, 3, 1, 1))
         self.register_buffer("std", torch.tensor([0.229, 0.224, 0.225]).view(1, 3, 1, 1))
 
-    def compute(self, input : torch.Tensor, target_style, target_content):
+    def prepare(self, input: torch.Tensor):
         input = input.repeat(1, 3, 1, 1)
-        target_style = target_style.repeat(1, 3, 1, 1)
-        target_content = target_content.repeat(1, 3, 1, 1)
-
         input = (input-self.mean) / self.std
-        target_style = (target_style-self.mean) / self.std
-        target_content = (target_content-self.mean) / self.std
-
         input = self.transform(input, mode='bilinear', size=(224, 224), align_corners=False)
-        target_style = self.transform(target_style, mode='bilinear', size=(224, 224), align_corners=False)
-        target_content = self.transform(target_content, mode='bilinear', size=(224, 224), align_corners=False)
+        
+    def loss_eval(self, input : torch.Tensor, target_style, target_content):
+        input = prepare(input)
+        target_style = prepare(target_style)
+        target_content = prepare(target_content)
 
         loss_content = 0.0
         loss_style = 0.0
@@ -125,19 +115,18 @@ class ConvNextPerceptualLoss(nn.Module):
 
         return loss_content, loss_style
 
-    def forward(self, input, target_style, target_content=None):
-        loss_content = torch.zeros((), requires_grad = True).to(device = self.device, non_blocking=False)
-        loss_style = torch.zeros((), requires_grad = True).to(device = self.device, non_blocking=False)
+    def forward(self, input, target_style, target_content):
+        loss_content = 0.0
+        loss_style = 0.0
         if len(input.shape)==4:
-            loss_content, loss_style = self.compute(input, target_style, target_content)
-            loss_content = loss_content*70 
-            loss_style = loss_style*70 
+            loss_content, loss_style = self.loss_eval(input, target_style, target_content)
         else:
             for i in range(input.shape[2]):
-                loss_content_tmp, loss_style_tmp = self.compute(input[0,0,i], target_style[0,0,i], target_content[0,0,i])
+                loss_content_tmp, loss_style_tmp = self.loss_eval(input[:,0,i], target_style[:,0,i], target_content[:,0,i])
                 loss_content += loss_content_tmp
                 loss_style += loss_style_tmp
-            loss_content = loss_content/input.shape[2]*70 
-            loss_style = loss_style/input.shape[2]*70 
-        # print("Loss content:", loss_content.item(), "Loss style:", loss_style.item())
+            loss_content /= input.shape[2] 
+            loss_style /= input.shape[2] 
+        loss_content*= 70
+        loss_style*= 70
         return loss_content+loss_style
